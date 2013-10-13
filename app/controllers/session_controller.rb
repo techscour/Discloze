b252fbba55f4f420dca4ae628281448315ec409f
George Maney <gmaney@ieee.org>
2013-10-13 12:32:49 -0700

    ok

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -15,15 +15,6 @@ class SessionController < ApplicationController
   end
 
   def mail
-    Mail.defaults do
-      delivery_method :smtp, { :address   => "smtp.sendgrid.net",
-                               :port      => 587,
-                               :domain    => "techscour.com",
-                               :user_name => ENV['MGNAME'],
-                               :password  => ENV['MGPASS'],
-                               :authentication => 'plain',
-                               :enable_starttls_auto => true }
-    end
     list = List.where(:name => params['list'], :public_id => session[:user_id]).first
     mail = Mail.new
     message = params['body'] + "\n\n" + format_list(list)
